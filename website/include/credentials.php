5424123ec504cfafaaa9fc20d9b621e62701c4fa
mj03382 <42617027+mj03382@users.noreply.github.com>
2018-11-29 14:34:55 -0500

    Add files via upload

---
+++ website/include/credentials.php
@@ -0,0 +1,13 @@
+<?php
+
+class Credentials {
+
+    // Declare the credentials to the database
+    public $servername = "mysqlforlambdatest.cjorddilq5ub.us-east-1.rds.amazonaws.com";
+    public $serverusername = "LambdaTestUser";
+    public $serverpassword = "808sandheartbreak";
+    public $serverdb = "IT5236DB";
+
+}
+
+?>
